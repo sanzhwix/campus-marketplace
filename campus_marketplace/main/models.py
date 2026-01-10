08ebd81327f8199745b5e205e092423eb2fbc41e
sserzhan <serzhanovsancho@gmail.com>
2026-01-10 13:32:57 +0500

    logic adding and basic html for the main page

--- campus_marketplace/main/models.py
+++ campus_marketplace/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+def get_absolute_url(self):
+    return reverse("main:product_list_by_category", args=[self.slug])
     
 
 
@@ -31,3 +35,7 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+
+
+def get_absolute_url(self):
+    return reverse("main:product_details", args=[self.id, self.slug])
